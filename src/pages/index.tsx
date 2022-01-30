1b6e72a1e7caaecc1c8fe637baf3b388a28c4225
Alan Maxwell <alanmaxwellantunes@gmail.com>
2022-01-30 10:25:36 -0300

    create Layout and Title components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,17 @@
+import Layout from "../components/Layout";
+
 export default function Home() {
-  return <div className={``}></div>;
+  return (
+    <div
+      className={`
+      flex justify-center items-center h-screen
+      bg-gradient-to-r from-blue-500 to-purple-500
+      text-white
+    `}
+    >
+      <Layout title="Registration">
+        <span>Content</span>
+      </Layout>
+    </div>
+  );
 }
