511f983fdb672250b8656ac9df82f138bb744e3d
Alan Maxwell <alanmaxwellantunes@gmail.com>
2022-02-18 22:37:05 -0300

    apply style on Table component

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -8,9 +8,9 @@ export default function Table(props: TableProps) {
   function renderHeader() {
     return (
       <tr>
-        <th>Code</th>
-        <th>Name</th>
-        <th>Age</th>
+        <th className="text-left p-4">Code</th>
+        <th className="text-left p-4">Name</th>
+        <th className="text-left p-4">Age</th>
       </tr>
     );
   }
@@ -18,18 +18,28 @@ export default function Table(props: TableProps) {
   function renderData() {
     return props.clients?.map((client, index) => {
       return (
-        <tr key={client.id}>
-          <td>{client.id}</td>
-          <td>{client.name}</td>
-          <td>{client.age}</td>
+        <tr
+          key={client.id}
+          className={`${index % 2 === 0 ? "bg-purple-200" : "bg-purple-100"}`}
+        >
+          <td className="text-left p-4">{client.id}</td>
+          <td className="text-left p-4">{client.name}</td>
+          <td className="text-left p-4">{client.age}</td>
         </tr>
       );
     });
   }
 
   return (
-    <table>
-      <thead>{renderHeader()}</thead>
+    <table className="w-full rounded-xl overflow-hidden">
+      <thead
+        className={`
+        text-gray-100
+          bg-gradient-to-r from-purple-500 to-purple-800
+        `}
+      >
+        {renderHeader()}
+      </thead>
       <tbody>{renderData()}</tbody>
     </table>
   );
